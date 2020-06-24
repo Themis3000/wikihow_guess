46f0024feabccf5cd7deacfb980f7a1d71188378
Themi <Tcm4760@gmail.com>
2020-06-23 20:47:05 -0500

    -added a partially completed articles_dir.txt file for test, will have broader categories in later versions -added the article page scraping, currently not fully realisable

--- build_img_dir.py
+++ build_img_dir.py
@@ -3,18 +3,20 @@ from bs4 import BeautifulSoup
 import time
 import json
 
-# delay between requests, in seconds
+# delay between requests in seconds
 REQUEST_DELAY = 2
 # Do scraping on sitemap page listing site categorys
-DO_CATEGORYS = True
+DO_CATEGORYS = False
 # Write scraped data to text file for later use (json format)
 WRITE_CATEGORYS = True
 # Do scraping on collected category listing pages
-DO_CATEGORY_PAGES = True
+DO_CATEGORY_PAGES = False
 # Write scraped data to text file for later use (json format)
 WRITE_CATEGORY_PAGES = True
 # Do scraping on collected article pages, also writes to text file for later use (json format)
 DO_PAGES = True
+# How many pages in to start
+PAGES_START = 1
 # delay between failed request retrys
 RETRY_DELAY = 20
 # amount of attempts to make before giving up
@@ -58,9 +60,9 @@ if DO_CATEGORY_PAGES:
     # request all category pages and get article links listed
     for i, category in enumerate(category_array):
         time.sleep(REQUEST_DELAY)
-        print(f'requesting {i+1}/{len(category_array)}')
         for retry in range(RETRY_ATTEMPTS):
             try:
+                print(f'requesting {i + 1}/{len(category_array)}')
                 category_soup = get_soup(category['link'])
                 print('Success')
                 break
@@ -83,3 +85,41 @@ if DO_CATEGORY_PAGES:
         save_data(category_page_array, 'category_page_dir.txt')
 
 if DO_PAGES:
+    if 'category_page_array' not in locals():
+        category_page_array = load_data('./category_page_dir.txt')
+
+    article_array = []
+    for i, article in enumerate(category_page_array[PAGES_START-1:]):
+        try:
+            time.sleep(REQUEST_DELAY)
+            for retry in range(RETRY_ATTEMPTS):
+                try:
+                    print(f'requesting {i+1}/{len(category_page_array)}')
+                    article_soup = get_soup(article["link"])
+                    print('Success')
+                    break
+                except:
+                    if retry+1 >= RETRY_ATTEMPTS:
+                        print(f"Request failed, retrying in {RETRY_DELAY} (attempt #{retry+1}/{RETRY_ATTEMPTS})")
+                        time.sleep(RETRY_DELAY)
+                    else:
+                        print("Maximum retry attempts reached, exiting...")
+                        save_data(article_array, 'articles_dir_unfinished.txt')
+                        exit()
+            image_containers = article_soup.find_all('a', class_='image')
+            article_images = []
+            for image_container in image_containers:
+                try:
+                    image = image_container.find('img')
+                    article_images.append(image['data-src'])
+                except:
+                    print('a problem occurred while scraping an image, skipping it...')
+            article_array.append({"title": article["title"], "link": article["link"], "category": article["category"],
+                                  "images": article_images})
+        except (KeyboardInterrupt, SystemExit):
+            print('saving first and exiting...')
+            save_data(article_array, 'articles_dir.txt')
+            raise
+        except:
+            print('a problem occurred while scraping an article, skipping over it...')
+    save_data(article_array, 'articles_dir.txt')
